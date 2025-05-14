46eeeab5272b808c02b0538552ff3af8796c154a
br3-mah <nyeleti.bremah@gmail.com>
2025-05-14 05:40:27 +0200

    Consignment tracking | Bulk SMS Update | twilio

--- Modules/Cargo/Resources/views/adminLte/pages/consignments/editor/table.blade.php
+++ Modules/Cargo/Resources/views/adminLte/pages/consignments/editor/table.blade.php
@@ -7,10 +7,13 @@
     <h2 class="text-lg font-bold text-primary tracking-tight">Consignment Tracking System</h2>
     <!-- Bulk Delete Button -->
     <div class="flex space-x-2">
-        <button id="bulk-delete-btn"
-            class="btnclicky px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 transition disabled:opacity-50"
+        <button id="bulk-delete-btn" 
+            class="btnclicky btn-sm d-flex item-center justify-content-center bg-red-500 text-white rounded hover:bg-red-600 transition disabled:opacity-50"
             disabled>
-            Bulk Delete
+            <span> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+            </svg></span>
         </button>
     </div>
   </div>
@@ -24,7 +27,7 @@
                 <input type="checkbox" class="bulk-checkbox" id="select-all">
             </th>
             <th class="px-6 py-3 font-semibold tracking-wider text-left">CODE</th>
-            <th class="px-6 py-3 font-semibold tracking-wider text-left">SHIPMENT PARCELS</th>
+        <th class="px-6 py-3 font-semibold tracking-wider text-left">SHIPMENT PARCELS</th>
             <th class="px-6 py-3 font-semibold tracking-wider text-left">CONSIGNEE</th>
             <th class="px-6 py-3 font-semibold tracking-wider text-left">SOURCE</th>
             <th class="px-6 py-3 font-semibold tracking-wider text-left">DESTINATION</th>
