956038db620792b74268fe1a17a9f01017fd0a72
br3-mah <nyeleti.bremah@gmail.com>
2025-05-24 13:41:08 +0200

    Last Work

--- resources/views/adminLte/components/aside.blade.php
+++ resources/views/adminLte/components/aside.blade.php
@@ -17,7 +17,7 @@
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="{{ auth()->user()->avatar ? Storage::url(auth()->user()->avatar) : asset('assets/lte/media/avatars/blank.png') }}" class="img-circle elevation-2" alt="User Image">
+                <img src="{{ auth()->user()->avatar ? url('storage/app/public/'.auth()->user()->avatar) : asset('assets/lte/media/avatars/blank.png') }}" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
                 <a href="#" class="d-block">{{ auth()->user()->name }} |
@@ -76,6 +76,8 @@
                         </ul>
                 </li>
                 @endcan
+
+                
                 @if (app('hook')->get('aside_menu'))
                     @foreach (aasort(app('hook')->get('aside_menu'), 'order') as $componentView)
                         {!! $componentView !!}
