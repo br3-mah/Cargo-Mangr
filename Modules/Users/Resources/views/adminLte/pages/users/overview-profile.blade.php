2c91435e673fe4399995f72383a5258ebc85b68e
br3-mah <nyeleti.bremah@gmail.com>
2025-05-24 13:54:01 +0200

    Last Work

--- Modules/Users/Resources/views/adminLte/pages/users/overview-profile.blade.php
+++ Modules/Users/Resources/views/adminLte/pages/users/overview-profile.blade.php
@@ -6,7 +6,7 @@
             <!--begin: Pic-->
             <div class="me-7 mb-4">
                 <div class="symbol symbol-100px symbol-lg-160px symbol-fixed position-relative">
-                    <img src="{{ $model->avatar ? Storage::url($model->avatar) : asset('assets/img/blank.png') }}" />
+                    <img src="{{ $model->avatar ? url('storage/app/public/'.$model->avatar) : asset('assets/img/blank.png') }}" />
                     <div class="position-absolute translate-middle bottom-0 start-100 mb-6 bg-success rounded-circle border border-4 border-white h-20px w-20px"></div>
                 </div>
             </div>
