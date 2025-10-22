5fa74e685458787df33e2dffabf47d28ecb084d2
br3-mah <nyeleti.bremah@gmail.com>
2025-10-22 11:57:37 +0200

    standard report

--- resources/views/adminLte/pages/reports/nwc/index.blade.php
+++ resources/views/adminLte/pages/reports/nwc/index.blade.php
@@ -188,9 +188,9 @@
                                     $mtnAmount = $row['mtn'] ?? 0;
                                     $cashPaymentsAmount = $row['cash_payments'] ?? 0;
                                 @endphp
-                                <td class="bg-danger text-dark">{{ $airtelAmount > 0 ? number_format($airtelAmount, 2) : '-' }}</td>
-                                <td class="bg-warning text-dark">{{ $mtnAmount > 0 ? number_format($mtnAmount, 2) : '-' }}</td>
-                                <td class="bg-success text-dark">{{ $cashPaymentsAmount > 0 ? number_format($cashPaymentsAmount, 2) : '-' }}</td>
+                                <td class="bg-danger text-dark" style="color:#000">{{ $airtelAmount > 0 ? number_format($airtelAmount, 2) : '-' }}</td>
+                                <td class="bg-warning text-dark" style="color:#000">{{ $mtnAmount > 0 ? number_format($mtnAmount, 2) : '-' }}</td>
+                                <td class="bg-success text-dark" style="color:#000">{{ $cashPaymentsAmount > 0 ? number_format($cashPaymentsAmount, 2) : '-' }}</td>
                             </tr>
                         @empty
                             <tr>
