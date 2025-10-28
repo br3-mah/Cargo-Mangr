8a38b448beb4cee04c90f9f49620152c745a4ef3
br3-mah <nyeleti.bremah@gmail.com>
2025-10-28 16:41:52 +0200

    Only Cashier records payment.

--- Modules/Cargo/Resources/views/adminLte/pages/shipments/show.blade.php
+++ Modules/Cargo/Resources/views/adminLte/pages/shipments/show.blade.php
@@ -133,11 +133,20 @@
                                 </button>
                             {{-- @endcan --}}
                         @else
-                            @can('confirm-shipment-payment')
+                            @php
+                                $user = auth()->user();
+                                $hasCashierRole = $user->hasRole(['cashier', 'cashiers']);
+                                $hasPermission = $user->can('confirm-shipment-payment');
+                            @endphp
+
+                            @if($hasCashierRole)
+                            {{-- @if($hasCashierRole || $hasPermission) --}}
                                 <button class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-gray-700 bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-400" onclick="openMarkPaidModal({{ $shipment->id }})">
                                 <i class="fas fa-check-circle mr-1"></i> Mark as Paid
                             </button>
-                            @endcan
+                            @else
+                                <small>Only Cashier confirms payment.</small>
+                            @endif
                         @endif
 
                         @if($user_role == $admin || auth()->user()->can('edit-shipments'))
